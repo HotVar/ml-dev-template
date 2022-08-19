7ab17a2c5b7cab0256937989eb5bb7cb9f360eb6
root <root@localhost.localdomain>
2022-08-19 17:36:55 +0900

    edit deploy.sh

--- new_project/www/deploy.sh
+++ new_project/www/deploy.sh
@@ -4,16 +4,16 @@
 path="/home/sample/www"
 
 # DROP DIST DIRECTORY
-rm -Rf ${path}/dist
+rm -Rf ${path}/script/dist
 
 # MAKE DIST DIRECTORY
-mkdir -p ${path}/dist
+mkdir -p ${path}/script/dist
 
 # COPY DIRECTORY
-cp -rf ${path}/src/* ${path}/dist
+cp -rf ${path}/script/src/* ${path}/script/dist
 
 # APPLY DEPLOY SETTINGS JSON PATH
-grep -rl 'deploy_settings_src.json' ${path}/dist | xargs sed -i 's/deploy_settings_src/deploy_settings_dist/g'
+grep -rl 'deploy_settings_src.json' ${path}/script/dist | xargs sed -i 's/deploy_settings_src/deploy_settings_dist/g'
 
 # SCRIPT DEPLOY END
 echo "SCRIPT DEPLOY COMPLETE";
